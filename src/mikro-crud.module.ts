55c3058d1702476ecf764bae5a8df9d41b85d8ee
Chihab Hajji <chiheb-gcbdev@gulfofficial.com>
2023-12-11 16:37:07 +0100

    upgrade

--- src/mikro-crud.module.ts
+++ src/mikro-crud.module.ts
@@ -1,27 +1,36 @@
-import { DynamicModule, Module } from "@nestjs/common";
-import { MikroCrudModuleOptions } from "./mikro-crud-module-options.interface";
-import { ENTITY_FILTERS, QueryParser } from "./providers";
+import {DynamicModule, Module, ValidationPipe} from "@nestjs/common";
+import {MikroCrudModuleOptions} from "./mikro-crud-module-options.interface";
+import {ENTITY_FILTERS, QueryParser} from "./providers";
+import {APP_PIPE} from "@nestjs/core";
 
 @Module({
-  providers: [
-    QueryParser,
-    { provide: ENTITY_FILTERS, useValue: (user: any) => ({ crud: { user } }) },
-  ],
-  exports: [QueryParser, ENTITY_FILTERS],
+    providers: [
+        QueryParser,
+        {provide: ENTITY_FILTERS, useValue: (user: any) => ({crud: {user}})},
+    ],
+    exports: [QueryParser, ENTITY_FILTERS],
 })
 export class MikroCrudModule {
-  // noinspection JSUnusedGlobalSymbols
-  static configure({ filters }: MikroCrudModuleOptions): DynamicModule {
-    return {
-      module: this,
-      providers: [
-        QueryParser,
-        {
-          provide: ENTITY_FILTERS,
-          useValue: filters,
-        },
-      ],
-      exports: [QueryParser, ENTITY_FILTERS],
-    };
-  }
+    // noinspection JSUnusedGlobalSymbols
+    static configure({filters}: MikroCrudModuleOptions): DynamicModule {
+        return {
+            module: this,
+            providers: [
+                QueryParser,
+                {
+                    provide: ENTITY_FILTERS,
+                    useValue: filters,
+                },
+                {
+                    provide: APP_PIPE,
+                    useClass: QueryParser,
+                },
+                {
+                    provide: APP_PIPE,
+                    useClass: ValidationPipe,
+                }
+            ],
+            exports: [QueryParser, ENTITY_FILTERS],
+        };
+    }
 }
